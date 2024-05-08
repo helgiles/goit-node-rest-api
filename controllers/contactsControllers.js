36321f019086bcbddbde9eca10c6ec94103649b4
Олег Слюсар <eternityonly1@gmail.com>
2024-05-08 10:15:28 +0300

    v-1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,77 @@
-import contactsService from "../services/contactsServices.js";
+import contactsService from '../services/contactsServices.js';
+import HttpError from '../helpers/HttpError.js';
+import {
+	createContactSchema,
+	updateContactSchema,
+} from '../schemas/contactsSchemas.js';
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+	const result = await contactsService.listContacts();
+	res.json(result);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const result = await contactsService.getContactById(id);
 
-export const deleteContact = (req, res) => {};
+		if (!result) {
+			throw HttpError(404);
+		}
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const result = await contactsService.removeContact(id);
 
-export const updateContact = (req, res) => {};
+		if (!result) {
+			throw HttpError(404);
+		}
+
+		res.status(201).json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const createContact = async (req, res, next) => {
+	try {
+		const contact = req.body;
+		const { error } = createContactSchema.validate(contact);
+
+		if (error) {
+			throw HttpError(400, error.message);
+		}
+
+		const result = await contactsService.addContact(contact);
+		res.status(201).json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const updateContact = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const contact = req.body;
+		const { error } = updateContactSchema.validate(contact);
+
+		if (error) {
+			throw HttpError(400, error.message);
+		}
+
+		const result = await contactsService.updateContact(id, contact);
+
+		if (!result) {
+			throw HttpError(404);
+		}
+		res.status(201).json(result);
+	} catch (error) {
+		next(error);
+	}
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,13 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-
-})
+	name: Joi.string().required().min(3).max(15),
+	email: Joi.string().email().required(),
+	phone: Joi.string().required().min(5).max(15),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+	name: Joi.string().required().min(3).max(15),
+	email: Joi.string().email().required(),
+	phone: Joi.string().required().min(5).max(15),
+});
