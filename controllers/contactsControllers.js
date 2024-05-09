35418adbf4b4ac6eabfdff24f606380536a20a05
Олег Слюсар <eternityonly1@gmail.com>
2024-05-09 13:21:25 +0300

    work

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,9 +1,5 @@
 import contactsService from '../services/contactsServices.js';
 import HttpError from '../helpers/HttpError.js';
-import {
-	createContactSchema,
-	updateContactSchema,
-} from '../schemas/contactsSchemas.js';
 
 export const getAllContacts = async (req, res) => {
 	const result = await contactsService.listContacts();
@@ -42,11 +38,6 @@ export const deleteContact = async (req, res, next) => {
 export const createContact = async (req, res, next) => {
 	try {
 		const contact = req.body;
-		const { error } = createContactSchema.validate(contact);
-
-		if (error) {
-			throw HttpError(400, error.message);
-		}
 
 		const result = await contactsService.addContact(contact);
 		res.status(201).json(result);
@@ -59,11 +50,6 @@ export const updateContact = async (req, res, next) => {
 	try {
 		const { id } = req.params;
 		const contact = req.body;
-		const { error } = updateContactSchema.validate(contact);
-
-		if (error) {
-			throw HttpError(400, error.message);
-		}
 
 		const result = await contactsService.updateContact(id, contact);
 
