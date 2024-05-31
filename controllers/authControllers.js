9994fe969f9f8e6dba570f71d47e06bfe7a736b6
Олег Слюсар <eternityonly1@gmail.com>
2024-05-31 17:09:46 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -109,6 +109,11 @@ export const updateSubscription = async (req, res, next) => {
 
 export const updateAvatar = async (req, res, next) => {
 	const id = req.user.id;
+
+	if (!req.file || !req.file.filename) {
+		next(HttpError(400, 'Avatar file must be added'));
+	}
+
 	const file = req.file.filename;
 	const tmpPath = req.file.path;
 
